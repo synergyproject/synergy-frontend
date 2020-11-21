7fec18b64a9b43c432f5a81aa32522584c7a115c
Ivan Goriachykh <40425890+GorIvan@users.noreply.github.com>
2020-11-21 22:23:11 +0200

    authorization component is ready

--- src/components/Authorization.vue
+++ src/components/Authorization.vue
@@ -1,74 +1,93 @@
 <template>
-    <div class="auhtorization-page">
-        <form class="form" method="POST" action="/login_form.php">
-            <div class="form_inner">
-                <h4 class="form_title">Добро пожаловать!</h4>
-
-                <div class="form_login">
-                    <label for="login" class="form_label">Введите логин</label>
-                    <div class="input_password">
-                        <input type="text" id="login" name="login" placeholder="Email" class="form_input">
-                    </div>
-                </div>
-
-                <div class="form_password">
-                    <label for="password" class="form_label">Введите пароль</label>
-                    <div class="input_help">
-                        <input type="password" id="password" name="password" placeholder="Password" class="form_input password">
-                        <input type="checkbox" id="password_check" class="check">
-                        <label for="password_check" class="label_check"></label>
-                    </div>
-                    
-                    <div class="help">
-                        <input type="checkbox" id="help_check" class="help_check">
-                        <label for="help_check" class="help_label">help</label>
-                        <div class="help_content">
-                            <p class="help_article">Длина пароля должна быть <span class="help_strong">не менее 8</span> и <span class="help_strong"> более 64</span> символов.</p>
-                            <p class="help_article">Пароль должен состоять из букв латинского алфавита <span class="help_strong">(A-z)</span>, арабских цифр <span class="help_strong">(0-9)</span> и специальных символов: <span class="help_strong">( . , : ; ? ! * + % - < > @ [ ] { } / \ _ {} $ # )</span>.</p>
-                            <p class="help_article">Обязательно должна быть одна буква верхнего регистра <span class="help_strong">А-Z</span>, одна буква нижнего регистра <span class="help_strong">а-z</span>, специальный символ и арабская цифра.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="form_password">
-                    <label for="password_submit" class="form_label">Повторите пароль</label>
-                    <div class="input_help">
-                        <input type="password" id="password_submit" name="password_submit" placeholder="Password" class="form_input password_submit">
-                        <input type="checkbox" id="submit_check" class="check">
-                        <label for="submit_check" class="label_check"></label>
-                    </div>
-                    <p class="attention">Неверный пароль</p>
-                </div>
-
-                <div class="password_forgot">
-                    <a href="" class="password_link">Забыли пароль?</a>
-                </div>
-                <div class="submit_form">
-                    <input type="submit" name="submit" value="Войти" class="auhtorization-button">
-                </div>
-            </div>	
-	    </form>
-    </div>
+	<div class="authorization-page" v-on:keyup.enter="authorization()">
+		<div class="greeting">
+			Добро пожаловать!
+		</div>		
+		<div class="authorization-main-block">
+			<div class="validation-form">
+				<div class="authorization-controls">
+					<div class="validation-block">
+						<div class="input-header">Введите логин</div>
+						<input type="text" maxlength="64" autocomplete="off" v-model="email">
+					</div>
+					<div class="validation-block">
+						<div class="eye" v-on:click="changeEye(false)">
+							<img src="../assets/img/icon_eye_close.png" v-show="this.eyePassVisible">
+							<img src="../assets/img/icon_eye_open.png" v-show="!this.eyePassVisible">
+						</div>
+						<div class="input-header">Введите пароль</div>
+						<input v-bind:type="this.inputTypePass" maxlength="64" autocomplete="off" v-model="pass">
+						<div class="input-basement">
+							<div class="input-basement__left">
+								<img src="../assets/img/ahtung_circle.png">
+								<span>{{info_message}}</span>
+							</div>
+						</div>
+					</div>
+					<div class="save-button basic-buttons" v-on:click="authorization()">
+						Войти
+					</div>
+				</div>
+				<div class="password-help">
+					<img src="../assets/img/help_circle.png" class="question-icon">
+					<div class="help-content">
+						<div class="help-content__text">
+							Длина пароля должна быть <b>не менее 8</b><br>
+							и <b>не более 64</b> символов.<br> 
+							<br>
+							Пароль должен состоять из букв латинского алфавита <b>(A-z)</b>,<br> 
+							арабских цифр <b>(0-9)</b><br> 
+							и специальных символов:
+							<b>( . , : ; ? ! * + % - < > @ [ ] { } ( ) / \ _ {} $ # )</b>.	
+							<br><br>
+							Обязательно должна быть одна буква верхнего регистра <b>А-Z</b>, одна буква
+							нижнего регистра <b>а-z</b>, специальный символ и арабская цифра.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>    
 </template>
 
 <script>
-    import help_circle from '../assets/img/help_circle.png';
+	import help_circle from '../assets/img/help_circle.png';
 	import ahtung_circle from '../assets/img/ahtung_circle.png';
 	import icon_eye_close from '../assets/img/icon_eye_close.png';
 	import icon_eye_open from '../assets/img/icon_eye_open.png';
 
-    export default {
-        name: 'auhtorization',
-        data () {
-            return {
-
-            }
-        },
-        components: {
-
-        },
-        methods: {
-
-        }
-    }
+	export default {
+		name: 'authorization',
+		data () {
+			return {
+                email: '',
+				pass: '',
+				info_message:'',
+				errors: {
+                    err_confirm: 'Неправильный адрес электронной почты или пароль',
+                    err_emptyField: 'Заполните все поля',
+                    good: 'Все ок' //test
+				},
+				eyePassVisible: true,
+				inputTypePass: 'password',
+			}
+		},
+		components: {
+	    },			
+	  	methods: {
+			authorization: function () {
+		  		if (this.email && this.pass) {
+                    this.info_message = this.errors.good
+                    //let UserLogin = JSON.stringify('в каком виде мы отсылаем это на бэк?');
+                    //axios ?
+                } else {
+                    this.info_message = this.errors.err_emptyField
+                }
+			},
+		  	changeEye: function (eye) {
+		  		this.eyePassVisible = !this.eyePassVisible;
+		  		this.inputTypePass = this.eyePassVisible ? 'password' : 'text'	  		
+			  }	  	
+		}  			
+	}
 </script>
