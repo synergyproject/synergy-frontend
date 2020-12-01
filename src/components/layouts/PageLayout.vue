b5c356c5c608305fa068737f6787216d304cffd8
Olena <elena.cerg@gmail.com>
2020-12-01 12:21:20 +0200

    routs+sidebar

---
+++ src/components/layouts/PageLayout.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="page">
+        <div class="page-sidebar">  
+            <div class="page-sidebar__exit">
+            </div>
+            <div class="avatar" >
+                <img src="@/assets/img/avatar.png">
+            </div>
+            <ul class="page-sidebar__menu">
+                <li class="page-sidebar__menu-item">
+                    <router-link to="/page" class="page-sidebar__menu-link"><span>Кабинет игрока</span></router-link>
+                </li>
+                <li class="page-sidebar__menu-item">
+                    <router-link to="/page/line" class="page-sidebar__menu-link"><span>Лента</span></router-link>
+                </li>
+                <li class="page-sidebar__menu-item">
+                    <router-link to="/page/goal" class="page-sidebar__menu-link"><span>Мои Цели</span></router-link>
+                </li>
+            </ul>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "PageLayout",
+        data () {
+            return {
+                
+            }
+        },
+    }
+
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Main from '../components/Main.vue'
 import Authorization from '../components/Authorization.vue'
+import PlayerPage from '../components/PlayerPage.vue'
+import PageLine from '../components/PageLine.vue'
+import PageGoal from '../components/PageGoal.vue'
 import Logout from '../components/Logout.vue'
 
 Vue.use(VueRouter)
@@ -30,7 +33,23 @@ const routes = [
 		path: '/logout',
 	    name: 'Logout',
 	    component: Logout		
+	},
+	{
+		path: '/page',
+	    name: 'PlayerPage ',
+	    component: PlayerPage 
+	},
+	{
+		path: '/page/line',
+	    name: 'PageLine',
+	    component: PageLine
+	},
+	{
+		path: '/page/goal',
+	    name: 'PageGoal',
+	    component: PageGoal
 	}
+
 ]
 
 const router = new VueRouter({
